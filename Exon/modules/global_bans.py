34a8a88286bb1f868dd9281b411b58dba014c710
kakashininja014 <112592194+kakashininja014@users.noreply.github.com>
2022-09-01 16:06:28 +0530

    Update global_bans.py

--- Exon/modules/global_bans.py
+++ Exon/modules/global_bans.py
@@ -591,7 +591,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "𝙰nti-sᴘᴀᴍ"
+__mod_name__ = "𝙰nti-sᴘᴀᴍ 👺"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set
