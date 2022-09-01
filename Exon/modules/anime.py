56fbf363287c39f56e5d131b03f96359a9e026f6
kakashininja014 <112592194+kakashininja014@users.noreply.github.com>
2022-09-01 14:39:39 +0530

    Update anime.py

--- Exon/modules/anime.py
+++ Exon/modules/anime.py
@@ -519,7 +519,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 # dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "𝙰ɴɪᴍᴇ"
+__mod_name__ = "𝙰ɴɪᴍᴇ ❤️"
 __command_list__ = [
     "anime",
     "manga",
