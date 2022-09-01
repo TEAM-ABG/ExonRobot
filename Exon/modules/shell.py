26190b28f8a8c396eca4f5f7445533f03fe765d4
kakashininja014 <112592194+kakashininja014@users.noreply.github.com>
2022-09-01 16:43:09 +0530

    Update shell.py

--- Exon/modules/shell.py
+++ Exon/modules/shell.py
@@ -71,6 +71,6 @@ def shell(update: Update, context: CallbackContext):
 
 SHELL_HANDLER = CommandHandler(["sh"], shell, run_async=True)
 dispatcher.add_handler(SHELL_HANDLER)
-__mod_name__ = "Shell"
+__mod_name__ = "Shell 👽"
 __command_list__ = ["sh"]
 __handlers__ = [SHELL_HANDLER]
