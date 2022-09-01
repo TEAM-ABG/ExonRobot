6343cd2ef1648c0032c15645cd2110d2207197de
kakashininja014 <112592194+kakashininja014@users.noreply.github.com>
2022-09-01 15:56:00 +0530

    Update dbcleanup.py

--- Exon/modules/dbcleanup.py
+++ Exon/modules/dbcleanup.py
@@ -205,5 +205,5 @@ BUTTON_HANDLER = CallbackQueryHandler(callback_button, pattern="db_.*", run_asyn
 dispatcher.add_handler(DB_CLEANUP_HANDLER)
 dispatcher.add_handler(BUTTON_HANDLER)
 
-__mod_name__ = "DB ᴄʟᴇᴀɴᴜᴘ"
+__mod_name__ = "DB ᴄʟᴇᴀɴᴜᴘ 🧹"
 __handlers__ = [DB_CLEANUP_HANDLER, BUTTON_HANDLER]
