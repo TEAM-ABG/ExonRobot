780826705cd66643a260800deb54bcadc0b2add7
kakashininja014 <112592194+kakashininja014@users.noreply.github.com>
2022-09-01 14:13:50 +0530

    Update helper.py

--- ABG/helper.py
+++ ABG/helper.py
@@ -1,6 +1,6 @@
-HELP_IMG = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+HELP_IMG = ["https://telegra.ph/file/2007f9e34a649fa2a2826.jpg"]
 
-START_IMG = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+START_IMG = ["https://telegra.ph/file/2007f9e34a649fa2a2826.jpg"]
 
 
-PHOTO = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+PHOTO = ["https://telegra.ph/file/2007f9e34a649fa2a2826.jpg"]
