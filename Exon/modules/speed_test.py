0e2077ffb07eb9f78f19423d27f2a7f8ae6a7e10
kakashininja014 <112592194+kakashininja014@users.noreply.github.com>
2022-09-01 16:46:34 +0530

    Update speed_test.py

--- Exon/modules/speed_test.py
+++ Exon/modules/speed_test.py
@@ -88,6 +88,6 @@ SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
 dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 
-__mod_name__ = "𝚂ᴘᴇᴇᴅᴛᴇsᴛ"
+__mod_name__ = "𝚂ᴘᴇᴇᴅᴛᴇsᴛ 🚄"
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
