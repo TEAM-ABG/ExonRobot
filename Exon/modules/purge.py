395d8894b2051e2c6c642819f9a10a2a4bc647f0
kakashininja014 <112592194+kakashininja014@users.noreply.github.com>
2022-09-01 16:34:01 +0530

    Update purge.py

--- Exon/modules/purge.py
+++ Exon/modules/purge.py
@@ -102,6 +102,6 @@ DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
 telethn.add_event_handler(*PURGE_HANDLER)
 telethn.add_event_handler(*DEL_HANDLER)
 
-__mod_name__ = "Purges"
+__mod_name__ = "Purges 😕"
 __command_list__ = ["del", "purge"]
 __handlers__ = [PURGE_HANDLER, DEL_HANDLER]
