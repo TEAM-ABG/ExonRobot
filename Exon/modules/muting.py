d742950126342e2b06c84d9a3911e75db8205056
kakashininja014 <112592194+kakashininja014@users.noreply.github.com>
2022-09-01 16:27:02 +0530

    Update muting.py

--- Exon/modules/muting.py
+++ Exon/modules/muting.py
@@ -350,5 +350,5 @@ dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_BUTTON_HANDLER)
 
-__mod_name__ = "ᴍᴜᴛɪɴɢ"
+__mod_name__ = "ᴍᴜᴛɪɴɢ 🤫"
 __handlers__ = [MUTE_HANDLER, UNMUTE_HANDLER, TEMPMUTE_HANDLER]
